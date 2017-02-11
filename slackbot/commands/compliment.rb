b2aee09a01af65cf0d617b10d9a841f1382f8fb0
OtherUser <jia.coding@gmail.com>
2017-02-11 15:00:17 -0500

    More emoji attempts

--- slackbot/commands/compliment.rb
+++ slackbot/commands/compliment.rb
@@ -1,4 +1,4 @@
-require './apis/emojis'
+require '../apis/emojis'
 
 module SlackComplimentBot
   module Commands
@@ -16,8 +16,13 @@ module SlackComplimentBot
       end
 
       match(/^emoji (?<emoji_key>\w*)\?$/i) do |client, data, match|
-        emojis = SlackWebClient::Emojis.get_emojis_with_name(match[:emoji_key])
+        slack_emojis = client.web_client.emoji_list[:emoji]
+        emoji_list = slack_emojis.map{|k,v| k}.uniq if slack_emojis.present?
+        emoji_list ||= []
+
+        emojis = emoji_list.select{ |e| e.include?(match[:emoji_key])}
         emoji_message = emojis.map{ |e| ':' + e }.join(' ')
+
         client.say(channel: data.channel, text: "threws a bunch of emojis: #{emoji_message} ")
       end
     end
