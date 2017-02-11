0b3328773e84425f19c29d4ce0aac0cb2a0c2998
nikymorg <nicole.morgan@gmail.com>
2017-02-11 14:59:54 -0500

    Clean flirt

--- slackbot/commands/flirt.rb
+++ slackbot/commands/flirt.rb
@@ -2,12 +2,10 @@ module SlackComplimentBot
   module Commands
     class Flirt < SlackRubyBot::Commands::Base
       command 'flirt with' do |client, data, _match|
-        # puts data
-        # username = data.text.split(" ").select do |word|
-        #   word.start_with?("<@") && word != "<@complimentbot"
-        # end
-        # p username[0][2...-2]
-        # user_id = find_user_id(username)
+        username = data.text.split(" ").find do |word|
+          word.start_with?("<@") && word != "<@complimentbot"
+        end
+        p username[2...-2]
         client.say(channel: 'C44TN4QMD', text: 'you all are cute')
       end
     end
