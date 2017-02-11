17cb7a567b733f9110fd17f6822f9a228fdaeaf2
nikymorg <nicole.morgan@gmail.com>
2017-02-11 14:09:19 -0500

    Remove slack api calls

--- slackbot/apis/slack.rb
+++ slackbot/apis/slack.rb
@@ -1,10 +1,10 @@
-require 'httparty'
+# require 'httparty'
 # require '../../config/secrets'
 
-def find_user_id(username)
-  url = "https://slack.com/api/users.list?token=#{ORG_TOKEN}&pretty=1"
-  response = HTTParty.get(url)
-  response_hash = response.to_h
-
-  response_hash["members"].find{|user| user["name"] == username}["id"]
-end
+# def find_user_id(username)
+#   url = "https://slack.com/api/users.list?token=#{ORG_TOKEN}&pretty=1"
+#   response = HTTParty.get(url)
+#   response_hash = response.to_h
+#
+#   response_hash["members"].find{|user| user["name"] == username}["id"]
+# end
