2762febc266cb5a14ac176bf28f20ef0a8285456
lshapz <laurashapiro01@gmail.com>
2017-02-11 15:15:19 -0500

    foobar

--- slackbot/commands/compliment.rb
+++ slackbot/commands/compliment.rb
@@ -16,15 +16,18 @@ module SlackComplimentBot
         client.say(channel: data.channel, text: emoji_list.to_s)
       end
 
-      match(/^emoji (?<emoji_key>\w*)\?$/i) do |client, data, match|
-        slack_emojis = client.web_client.emoji_list[:emoji]
-        emoji_list = slack_emojis.map{|k,v| k}.uniq if slack_emojis.present?
-        emoji_list ||= []
+      match(/^emoji (?<emoji_key>\w*)\$/i) do |client, data, match|
+        client.say(channel:data.channel, text: match[:emoji_key])
+        # slack_emojis = client.web_client.emoji_list[:emoji]
+        # p data.text
+        # p match
+        # emoji_list = slack_emojis.map{|k,v| k}.uniq if slack_emojis.present?
+        # emoji_list ||= []
 
-        emojis = emoji_list.select{ |e| e.include?(match[:emoji_key])}
-        emoji_message = emojis.map{ |e| ':' + e }.join(' ')
+        # emojis = emoji_list.select{ |e| e.include?(match[:emoji_key])}
+        # emoji_message = emojis.map{ |e| ':' + e }.join(' ')
 
-        client.say(channel: data.channel, text: "threws a bunch of emojis: #{emoji_message} ")
+        # client.say(channel: data.channel, text: "threws a bunch of emojis: #{emoji_message} ")
       end
     end
   end
