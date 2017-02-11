62e84bae79b09008f11407c97e458a6e2ca2dd82
nikymorg <nicole.morgan@gmail.com>
2017-02-11 14:07:58 -0500

    Switch flirt channel

--- slackbot/commands/flirt.rb
+++ slackbot/commands/flirt.rb
@@ -1,14 +1,13 @@
-require 'byebug'
 module SlackComplimentBot
   module Commands
     class Flirt < SlackRubyBot::Commands::Base
       command 'flirt with' do |client, data, _match|
         # puts data
-        username = data.text.split(" ").select do |word|
-          word.start_with?("@") && word != "@complimentbot"
-        end
+        # username = data.text.split(" ").select do |word|
+        #   word.start_with?("@") && word != "@complimentbot"
+        # end
         # user_id = find_user_id(username)
-        client.say(channel: username, text: "you're cute")
+        client.say(channel: data.user, text: "you're cute")
       end
     end
   end
