ddc593a132ab245f3d68daecfa3c1dbc2cf0a939
lshapz <laurashapiro01@gmail.com>
2017-02-11 12:50:11 -0500

    inspire

--- slackbot/commands/inspire.rb
+++ slackbot/commands/inspire.rb
@@ -4,11 +4,12 @@ require 'httparty'
 module SlackComplimentBot
   module Commands
     class Inspire < SlackRubyBot::Commands::Base
+       
+      command 'inspire' do |client, data, _match|
         url = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
         response = HTTParty.get(url)
         quote = response.parsed_response["quoteText"]
-
-      command 'inspire' do |client, data, _match|
+        # sleep 1
         client.say(channel: data.channel, text: quote)
       end
     end
