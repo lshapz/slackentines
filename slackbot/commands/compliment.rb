912a7bdb0939de592bc481fc9be1c444b1c46f63
OtherUser <jia.coding@gmail.com>
2017-02-11 14:33:32 -0500

    Fix emoji call

--- slackbot/commands/compliment.rb
+++ slackbot/commands/compliment.rb
@@ -12,11 +12,11 @@ module SlackComplimentBot
       end
 
       command 'emojis' do |client, data, _match|
-        client.say(channel: data.channel, text: SlackWebClient.Emojis.get_emojis.to_s)
+        client.say(channel: data.channel, text: SlackWebClient::Emojis.get_emojis.to_s)
       end
 
       match(/^emoji (?<emoji_key>\w*)\?$/i) do |client, data, match|
-        emojis = SlackWebClient.Emojis.get_emojis_with_name(match[:emoji_key])
+        emojis = SlackWebClient::Emojis.get_emojis_with_name(match[:emoji_key])
         emoji_message = emojis.map{ |e| ':' + e }.join(' ')
         client.say(channel: data.channel, text: "threws a bunch of emojis: #{emoji_message} ")
       end
