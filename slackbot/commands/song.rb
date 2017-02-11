3ddae6564f60a2a0f9023fc11b44d8105e8b4a3b
krissasantos <krissa.santos31@gmail.com>
2017-02-11 15:11:09 -0500

    back to sampling from the array

--- slackbot/commands/song.rb
+++ slackbot/commands/song.rb
@@ -1,21 +1,22 @@
 
-require 'httparty'
+# require 'httparty'
 
 module SlackComplimentBot
   module Commands
     class Song < SlackRubyBot::Commands::Base
       #publicplaylist
-      @songs = []
-      url = https://api.spotify.com/v1/browse/categories/mood/playlists  
-      response = HTTParty.get(url)
-      output = response.parsed_response
+      # @songs = []
+      # url = "https://api.spotify.com/v1/browse/categories/mood/playlists" 
+      # response = HTTParty.get(url)
+      # output = response.parsed_response
 
-      output.data[0].each do |track|
-        @songs << track
-      end
-      # @songs = ["https://play.spotify.com/track/7MXVkk9YMctZqd1Srtv4MB", 
-      #   "https://play.spotify.com/track/1Tt4sE4pXi57mTD1GCzsqm", 
-      #   "https://play.spotify.com/track/4JehYebiI9JE8sR8MisGVb"]
+      # output.data[0].each do |track|
+      #   @songs << track
+      # end
+
+      @songs = ["https://play.spotify.com/track/7MXVkk9YMctZqd1Srtv4MB", 
+        "https://play.spotify.com/track/1Tt4sE4pXi57mTD1GCzsqm", 
+        "https://play.spotify.com/track/4JehYebiI9JE8sR8MisGVb"]
 
       command 'sing to me paolo', 'Sing to me Paolo' do |client, data, _match|
         client.say(channel: data.channel, text: @songs.sample)
