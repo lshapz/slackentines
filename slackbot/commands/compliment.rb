1959772a08d671237a3a9465578e70cb21c414bd
lshapz <laurashapiro01@gmail.com>
2017-02-11 17:07:23 -0500

    cleaning

--- slackbot/commands/compliment.rb
+++ slackbot/commands/compliment.rb
@@ -17,12 +17,6 @@ module SlackComplimentBot
         client.say(channel: data.channel, text: "https://twitter.com/gdinyc")
       end
 
-
-      command 'commands', 'help' do |client, data, _match|
-        client.say(channel: data.channel, text: "try: sing, emojis, hack, high five, compliment, inspire me, reassure me, gif me, kitten, pupper, or emo + a word that might be in an emoji name")
-      end
-
-
       command 'emojis' do |client, data, _match|
         slack_emojis = client.web_client.emoji_list[:emoji]
         emoji_list = slack_emojis.map{|k,v| k}.uniq if slack_emojis.present?
