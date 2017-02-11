0132cefce91e03610de4dfffa21e7ffb5ad635aa
krissasantos <krissa.santos31@gmail.com>
2017-02-11 16:07:58 -0500

    complete options of commands

--- slackbot/commands/help.rb
+++ slackbot/commands/help.rb
@@ -2,8 +2,16 @@ module SlackComplimentBot
   module Commands
     class Gif < SlackRubyBot::Commands::Base
       command 'gif me' do |client, data, _match|
-        client.say(channel: data.channel, text: "Options: 'CAT', 'cat', 'kitty', 'kitty!', 'kitten',
-          'inspire', 'reassure me', 'compliment', 'spill', 'emojis','sing to me paolo', 'Sing to me Paolo', 'gif me' ")
+        client.say(channel: data.channel, text: 
+          "Options: /n
+          command 'CAT', 'cat', 'kitty', 'kitty!', 'kitten',/n
+          command 'inspire'/n 
+          command 'reassure me'/n'
+          command compliment'/n 
+          command 'spill' /n
+          command 'emojis' /n
+          command 'sing to me paolo', 'Sing to me Paolo'/n
+          command 'gif me' ")
       end
     end
   end
