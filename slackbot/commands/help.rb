ff02b719c960607bf81a1441c8570c3a64e585a6
nikymorg <nicole.morgan@gmail.com>
2017-02-11 23:59:58 -0500

    Update help

--- slackbot/commands/help.rb
+++ slackbot/commands/help.rb
@@ -2,10 +2,9 @@ module SlackComplimentBot
   module Commands
     class Help < SlackRubyBot::Commands::Base
       command 'help me', 'help', 'commands' do |client, data, _match|
-        client.say(channel: data.channel, text: "Options: 'CAT', 'cat', 'kitty', 'kitty!', 'kitten', 
-          'dog', 'doggo', 'pupper', 'puppy!', 'puppy', 'DOG', 'compliment', 'inspire', 'reassure me', 'emojis','sing to me paolo', 'Sing to me Paolo', 'sing', 'gif me', 'emojis', 'emo + [word in emoji name]', or 'hackathon'")
+        client.say(channel: data.channel, text: "Options: 'CAT', 'cat', 'kitty', 'kitty!', 'kitten',
+          'dog', 'doggo', 'pupper', 'puppy!', 'puppy', 'DOG', 'compliment', 'inspire', 'reassure me', 'emojis','sing to me paolo', 'Sing to me Paolo', 'sing', 'gif me', 'emojis', 'emo + [word in emoji name]', 'hackathon' or 'flirt with + [user w/o @]'")
       end
     end
   end
 end
-
