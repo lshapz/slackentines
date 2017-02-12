1fd73c2f614fe394ab9044c1610a19da5c967029
nikymorg <nicole.morgan@gmail.com>
2017-02-11 23:32:22 -0500

    Fix for public channels

--- slackbot/commands/flirt.rb
+++ slackbot/commands/flirt.rb
@@ -4,7 +4,7 @@ module SlackComplimentBot
       command 'flirt with' do |client, data, _match|
         flirt_text = data.text
         if flirt_text.include?("@complimentbot")
-          flirt.text = flirt_text.slice!("@complimentbot ")
+          flirt_text.slice!("@complimentbot ")
         end
           flirt_text.slice!("flirt with ")
           text_array = flirt_text.split(" ")
